fa41ec9d0e74bbce1db1a0c7b51d5a8ec2d83be5
elastic7327 <elastic7327@gmail.com>
2018-08-07 21:08:40 +0900

    Cmt

--- pyrust-proof-example/src/lib.rs
+++ pyrust-proof-example/src/lib.rs
@@ -9,12 +9,12 @@ use self::crypto::sha2::Sha256;
 use self::crypto::digest::Digest;
 
 
- pub fn valid_proof(last_proof: u32, proof: u32, diff: u32) -> bool {
+ pub fn valid_proof(last_proof: u32, proof: u32, diff: usize) -> bool {
     let mut hasher = Sha256::new();
     let guess = &format!("{}{}", last_proof, proof);
     hasher.input_str(guess);
     let output = hasher.result_str();
-    &output[..diff as usize] == "0".repeat(diff as usize)
+    &output[..diff] == "0".repeat(diff)
 }
 
  fn example(_py: Python, diff: u32) -> PyResult<u32>{
@@ -22,7 +22,7 @@ use self::crypto::digest::Digest;
     let mut proof = 0;
     let last_proof = 0;
 
-    while valid_proof(last_proof, proof, diff) == false {
+    while valid_proof(last_proof, proof, diff as usize) == false {
          proof = proof + 1
     }
 
@@ -33,9 +33,3 @@ py_module_initializer!(while_with_sha, initexample, PyInit_example, |py, m| {
     m.add(py, "example", py_fn!(py, example(diff: u32)))?;
     Ok(())
 });
-
-//py_module_initializer!(example, initexample, PyInit_example, |py, m| {
-    //m.add(py, "count_doubles", py_fn!(py, count_doubles(var: &str)))?;
-    //Ok(())
-/*}*/
-
